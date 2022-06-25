24a82d25388a9be91c44c0e740d8efda44e9c519
justin-s <justin.s@kakaocorp.com>
2022-06-25 17:00:21 +0900

    init: project

---
+++ src/main/java/liquidjoo/reinventthewheel/ReinventthewheelApplication.java
@@ -0,0 +1,13 @@
+package liquidjoo.reinventthewheel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinventthewheelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReinventthewheelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/liquidjoo/reinventthewheel/ReinventthewheelApplicationTests.java
@@ -0,0 +1,13 @@
+package liquidjoo.reinventthewheel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinventthewheelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
